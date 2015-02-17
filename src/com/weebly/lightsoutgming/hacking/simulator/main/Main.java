63534b8412c1531322aa021de221be4f2780a861
Taz40 <Taz40@Taz40-PC>
2015-02-17 12:29:37 -0700

    initial commit

---
+++ src/com/weebly/lightsoutgming/hacking/simulator/main/Main.java
@@ -0,0 +1,37 @@
+package com.weebly.lightsoutgming.hacking.simulator.main;
+
+import io.brace.lightsoutgaming.engine.LightsOut;
+import io.brace.lightsoutgaming.engine.Network.NetworkUtils;
+
+public class Main extends LightsOut {
+	
+	public static void main(String[] args){
+		new Main().init();
+	}
+
+	@Override
+	protected void init() {
+		// TODO Auto-generated method stub
+		this.createDisplay("Hacking Simulator", 800, 600);
+		this.start();
+	}
+
+	@Override
+	protected void render() {
+		// TODO Auto-generated method stub
+		screen.clear();
+		for(int i = 0; i < NetworkUtils.myObjects.size(); i++){
+			NetworkUtils.myObjects.get(i).render(screen);
+		}
+		this.show();
+	}
+
+	@Override
+	protected void update() {
+		// TODO Auto-generated method stub
+		for(int i = 0; i < NetworkUtils.myObjects.size(); i++){
+			NetworkUtils.myObjects.get(i).update();
+		}
+	}
+	
+}
